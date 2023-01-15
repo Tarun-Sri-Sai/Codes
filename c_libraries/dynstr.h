6ea7a51ec0274f6ef43c53b906ef6c9caab0c92f
Tarun <tarunsrisaiv03@gmail.com>
2023-01-15 07:47:48 +0530

    Documentation Changes using wrapped text

--- c_libraries/dynstr.h
+++ c_libraries/dynstr.h
@@ -1,64 +1,61 @@
 #pragma once
 
 #include <stdio.h>
-#include <string.h>
+#include <String.h>
 #include <stdlib.h>
 
 /**
- * @brief   
- * @brief   Dynamic String Library
- * @details String grows and shrinks based on requirement.
- *          Allocation and deallocation of the object are explicit.
- *          Uses pointer to structs of type 'string *' that contain
- *          three items: val, length & capacity. capacity of 'string *'
- *          object changes with change in length.
- *          If only library's functions are used for access and
- *          modification, 'string *' objects are memory-safe.
+ * @brief   Dynamic String Object.
+ * @details String grows and shrinks based on requirement. Allocation and 
+ *          deallocation of the object are explicit. Contains three items:
+ *          val, length & capacity. Capacity of String object changes with
+ *          change in length. If only library's functions are used for
+ *          access and modification, String objects are memory-safe.
  */
-typedef struct string_tag
+typedef struct String_tag
 {
     char *val;
-    unsigned long length, capacity;
-} string;
+    int length, capacity;
+} String;
 
-string *init(char *val);
-string *create();
-void append(string **source, char *appendix);
-void extend(string **source, string **extension);
-void discard(string **source);
-void debug_print(string **source);
-void push_back(string **source, char appendix);
-void pop_back(string **source);
-const char *to_string(string **source);
-unsigned long length(string **source);
-void nappend(string **source, char *appendix, unsigned long max_size);
-void nextend(string **source, string **extension, unsigned long max_size);
-unsigned long min(unsigned long a, unsigned long b);
+String *init(char *val);
+String *create();
+void append(String **source, char *appendix);
+void extend(String **source, String **extension);
+void discard(String **source);
+void debug_print(String **source);
+void push_back(String **source, char appendix);
+void pop_back(String **source);
+const char *to_string(String **source);
+int length(String **source);
+void nappend(String **source, char *appendix, int max_size);
+void nextend(String **source, String **extension, int max_size);
+int min(int a, int b);
 
 /**
- * @brief   String object initializer
- * @details Creates a new object of type 'string *' and appends
- *          the char string val to the object's val item.
+ * @brief   String object initializer.
+ * @details Creates a new String object and appends the char-string val to 
+ *          the object's val item.
  *
- * @param   val char * 
- * @return  string *
+ * @param   val A char-string to initialize the String object with.
+ * @return  A pointer to String object.
  */
-string *init(char *val)
+String *init(char *val)
 {
-    string *retval = create();
+    String *retval = create();
     append(&retval, val);
     return retval;
 }
 
 /**
- * @brief   String object constructor
- * @details Creates a new object of type 'string *'.
+ * @brief   String object constructor.
+ * @details Creates a new String object.
  *
- * @return  string *
+ * @return   A pointer to String object.
  */
-string *create()
+String *create()
 {
-    string *retval = (string *)malloc(sizeof(string *));
+    String *retval = (String *)malloc(sizeof(String *));
     retval->capacity = 2;
     retval->length = 0;
     retval->val = (char *)calloc(retval->capacity + 1, sizeof(char));
@@ -67,22 +64,22 @@ string *create()
 }
 
 /**
- * @brief   
- * @details Appends char string of type 'char *' to the object pointed by
- *          source's val item.
- *          Does nothing if object pointed by source is (string *)0.
+ * @brief   Appends a char-string to String object.
+ * @details Appends char-string of type 'char *' to the object pointed by 
+ *          source's val item. Does nothing if object pointed by source is
+ *          (String *)0.
  *
- * @param   source      string ** 
- * @param   appendix    char * 
+ * @param   source      A pointer to pointer to String.
+ * @param   appendix    char-string to append.
  * @return  void
  */
-void append(string **source, char *appendix)
+void append(String **source, char *appendix)
 {
-    if ((*source) == ((string *)0))
+    if ((*source) == ((String *)0))
     {
         return;
     }
-    unsigned long i, length = strlen(appendix);
+    int i, length = strlen(appendix);
     for (i = 0; i < length; ++i)
     {
         char t = appendix[i];
@@ -97,24 +94,22 @@ void append(string **source, char *appendix)
 }
 
 /**
- * @brief   Appends one string object to another
- * @details object pointed by the val item of object pointed by extension
- *          to the val item of object pointed by source. The object pointed by
- *          extension remains unchanged.
- *          Does nothing if any one of source or extension point to
- *          (string *)0.
+ * @brief   Appends one String object to another.
+ * @details Appends String object source to String object extension. The 
+ *          object pointed by extension remains unchanged. Does nothing if
+ *          any one of source or extension point to (String *)0.
  *
- * @param   source      string **
- * @param   extension   string **
+ * @param   source      A pointer to pointer to String.
+ * @param   extension   A pointer to pointer to String.
  * @return  void
  */
-void extend(string **source, string **extension)
+void extend(String **source, String **extension)
 {
-    if ((*source) == ((string *)0) || (*extension) == ((string *)0))
+    if ((*source) == ((String *)0) || (*extension) == ((String *)0))
     {
         return;
     }
-    unsigned long i;
+    int i;
     for (i = 0; i < (*extension)->length; ++i)
     {
         char t = (*extension)->val[i];
@@ -129,37 +124,36 @@ void extend(string **source, string **extension)
 }
 
 /**
- * @brief   String object Destructor
- * @details Frees the memory of object pointed by source as well as its
- *          val item. Sets the object to (string *)0.
- *          Does nothing if the object pointed by source is
- *          (string *)0.
+ * @brief   String object Destructor.
+ * @details Appends String object source to String object extension. The 
+ *          object pointed by extension remains unchanged. Does nothing if
+ *          any one of source or extension point to (String *)0.
  *
- * @param   source  string **
+ * @param   source  A pointer to pointer to String.
  * @return  void
  */
-void discard(string **source)
+void discard(String **source)
 {
-    if (*source == ((string *)0))
+    if (*source == ((String *)0))
     {
         return;
     }
     free((*source)->val);
     free(*source);
-    *source = ((string *)0);
+    *source = ((String *)0);
 }
 
 /**
- * @brief   Debugger Print a String object
- * @details Prints the items in object pointed by source.
- *          Does nothing if source is pointing to (string *)0).
- *
- * @param   source  string ** 
+ * @brief   Debugger print a String object.
+ * @details Prints the items in object pointed by source. Does nothing if 
+ *          source is pointing to (String *)0).
+ * 
+ * @param   source  A pointer to pointer to String.
  * @return  void
  */
-void debug_print(string **source)
+void debug_print(String **source)
 {
-    if ((*source) == ((string *)0))
+    if ((*source) == ((String *)0))
     {
         return;
     }
@@ -167,18 +161,17 @@ void debug_print(string **source)
 }
 
 /**
- * @brief   Adds a character to String object
- * @details Appends the character appendix to the val item of the object
- *          pointed  by source.
- *          Does nothing if source is pointing to (string *)0).
- *
- * @param   source      string **
- * @param   appendix    char
+ * @brief   Adds a character at the end of String object.
+ * @details Prints the items in object pointed by source. Does nothing if 
+ *          source is pointing to (String *)0).
+ * 
+ * @param   source      A pointer to pointer to String.
+ * @param   appendix    A character to append.
  * @return  void
  */
-void push_back(string **source, char appendix)
+void push_back(String **source, char appendix)
 {
-    if ((*source) == ((string *)0))
+    if ((*source) == ((String *)0))
     {
         return;
     }
@@ -196,25 +189,24 @@ void push_back(string **source, char appendix)
 }
 
 /**
- * @brief   Delete character at end
- * @details Removes last character from object pointed by source.
- *          Discards old object and creates a new object if only one
- *          character is left in the val item of source.
- *          Does nothing if the object pointed by source is
- *          (string *)0.
- *
- * @param   source  string **
+ * @brief   Deletes character at the end of String object.
+ * @details Removes last character from object pointed by source. Discards 
+ *          old object and creates a new object if only one character is
+ *          left in the val item of source. Does nothing if the object
+ *          pointed by source is (String *)0.
+ * 
+ * @param   source  A pointer to pointer to String.
  * @return  void
  */
-void pop_back(string **source)
+void pop_back(String **source)
 {
-    if (*source == ((string *)0))
+    if (*source == ((String *)0))
     {
         return;
     }
     if ((*source)->length == 0)
     {
-        string *temp = *source;
+        String *temp = *source;
         *source = create();
         discard(&temp);
         return;
@@ -228,16 +220,16 @@ void pop_back(string **source)
 }
 
 /**
- * @brief   Return char pointer of String object
- * @details Returns val item of object pointed by source.
- *          Returns (char *)0 if source is pointing to (string *)0.
- *
- * @param   source  string **
- * @return  const char *
+ * @brief   Returns char pointer of String object.
+ * @details Returns val item of object pointed by source. Returns (char *)0 
+ *          if source is pointing to (String *)0.
+ * 
+ * @param   source  A pointer to pointer to String.
+ * @return  A const char-string version of String object.
  */
-const char *to_string(string **source)
+const char *to_string(String **source)
 {
-    if ((*source) == ((string *)0))
+    if ((*source) == ((String *)0))
     {
         return ((char *)0);
     }
@@ -245,16 +237,15 @@ const char *to_string(string **source)
 }
 
 /**
- * @brief   Returns length of String object
- * @details length item of object pointed by source. Returns 0 if
- *          source is pointing to (string *)0.
+ * @brief   Returns length of String object.
+ * @details Returns length item of String object.
  *
- * @param   source  string **
- * @return  unsigned long
+ * @param   source  A pointer to pointer to String.
+ * @return  Length of type int.
  */
-unsigned long length(string **source)
+int length(String **source)
 {
-    if ((*source) == ((string *)0))
+    if ((*source) == ((String *)0))
     {
         return 0;
     }
@@ -262,23 +253,23 @@ unsigned long length(string **source)
 }
 
 /**
- * @brief   Limited length version of append()
- * @details Appends atmost max_size characters from char string of type
- *          'char *' to the val item of object pointed by source.
- *          Does nothing if source is pointing to (string *)0.
+ * @brief   Length limited version of append().
+ * @details Appends char-string of type 'char *' to the object pointed by 
+ *          source's val item. Does nothing if object pointed by source is
+ *          (String *)0.
  *
- * @param   source      string **
- * @param   appendix      char *
- * @param   max_size      unsigned long
+ * @param   source      A pointer to pointer to String.
+ * @param   appendix    A char-string to append.
+ * @param   max_size    Max allowed length to append.
  * @return  void
  */
-void nappend(string **source, char *appendix, unsigned long max_size)
+void nappend(String **source, char *appendix, int max_size)
 {
-    if ((*source) == ((string *)0))
+    if ((*source) == ((String *)0))
     {
         return;
     }
-    unsigned long i, length = min(strlen(appendix), max_size);
+    int i, length = min(strlen(appendix), max_size);
     for (i = 0; i < length; ++i)
     {
         char t = appendix[i];
@@ -293,25 +284,23 @@ void nappend(string **source, char *appendix, unsigned long max_size)
 }
 
 /**
- * @brief   Limited length version of extend()
- * @details Appends the val item of object pointed by extension to the val
- *          item of object pointed by source. The object extension remains
- *          unchanged.
- *          Does nothing if any one of source or extension is pointing to
- *          (string *)0.
+ * @brief   Length limited version of extend()
+ * @details Appends String object source to String object extension. The 
+ *          object pointed by extension remains unchanged. Does nothing if
+ *          any one of source or extension point to (String *)0.
  *
- * @param   source      string **
- * @param   extension   string **
- * @param   max_size    unsigned long
+ * @param   source      A pointer to pointer to String.
+ * @param   appendix    A pointer to pointer to String.
+ * @param   max_size    Max allowed length to append.
  * @return  void
  */
-void nextend(string **source, string **extension, unsigned long max_size)
+void nextend(String **source, String **extension, int max_size)
 {
-    if ((*source) == ((string *)0) || (*extension) == ((string *)0))
+    if ((*source) == ((String *)0) || (*extension) == ((String *)0))
     {
         return;
     }
-    unsigned long i;
+    int i;
     for (i = 0; i < min((*extension)->length, max_size); ++i)
     {
         char t = (*extension)->val[i];
@@ -328,11 +317,11 @@ void nextend(string **source, string **extension, unsigned long max_size)
 /**
  * @brief   Returns minimum of a and b.
  *
- * @param   a   unsigned long
- * @param   b   unsigned long
- * @return  unsigned long
+ * @param   a   Integer of type int
+ * @param   b   Integer of type int
+ * @return  Integer of type int
  */
-unsigned long min(unsigned long a, unsigned long b)
+int min(int a, int b)
 {
     return a < b ? a : b;
 }
