7cae103273233bfa1cf45276d9c77e9d38f7de96
Tarun <tarunsrisaiv03@gmail.com>
2023-03-13 22:39:28 +0530

    Add custom input

---
+++ Runtime-Tests/a_star_vs_depth_limited/src/write_graph.py
@@ -0,0 +1,34 @@
+def get_info(line):
+    if line == "None":
+        return "None"
+    
+    result = ""
+    trimmed_line = line[1:-1]
+    vertex_pairs = trimmed_line.split(", ")
+    
+    for vertex_pair in vertex_pairs:
+        tokens = vertex_pair.split(": ")
+        result += str(tokens[0][1:-1] + ',' + tokens[1] + ' ')
+
+    return result[:-1]
+
+
+def main():
+    with open("../graphs/26nodes.txt", "w") as fwrite:
+        n_vertices = int(input())
+        fwrite.write(str(n_vertices) + "\n")
+        fwrite.write(str(2) + "\n")
+
+        for _ in range(n_vertices):
+            line = input()
+            line_trimmed = line[3::]
+            fwrite.write(get_info(line_trimmed) + "\n")
+
+        fwrite.write(input() + "\n")
+        fwrite.write(input() + "\n")
+        fwrite.write(str(int(input())) + "\n")
+        fwrite.write(str(int(input())) + "\n")
+
+
+if __name__ == "__main__":
+    main()
