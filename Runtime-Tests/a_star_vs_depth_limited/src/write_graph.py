9b8ea7a7702f22efb0ded4243e84342002a0fbd6
Tarun <tarunsrisaiv03@gmail.com>
2023-03-14 14:15:21 +0530

    Input queries and fix bug

--- Runtime-Tests/a_star_vs_depth_limited/src/write_graph.py
+++ Runtime-Tests/a_star_vs_depth_limited/src/write_graph.py
@@ -1,33 +1,33 @@
 def get_info(line):
     if line == "{}":
         return "None"
-    
+
     result = ""
     trimmed_line = line[1:-1]
-    vertex_pairs = trimmed_line.split(",\\s+")
-    
+    vertex_pairs = trimmed_line.split(", ")
+
     for vertex_pair in vertex_pairs:
-        tokens = vertex_pair.split(":\\s+")
-        result += str(tokens[0][1:-1] + ',' + tokens[1] + ' ')
+        tokens = vertex_pair.split(": ")
+        result += str(tokens[0] + ',' + tokens[1].strip() + ' ')
 
     return result[:-1]
 
 
 def main():
-    with open("../graphs/26nodes.txt", "w") as fwrite:
-        n_vertices = int(input())
+    with open("../graphs/test.txt", "w") as fwrite:
+        n_vertices = int(input("Enter no of vertices: "))
         fwrite.write(str(n_vertices) + "\n")
         fwrite.write(str(2) + "\n")
 
-        for _ in range(n_vertices):
-            line = input()
+        for i in range(n_vertices):
+            line = input(f"Enter line {i + 1}: ")
             line_trimmed = line[3::]
             fwrite.write(get_info(line_trimmed) + "\n")
 
-        fwrite.write(input() + "\n")
-        fwrite.write(input() + "\n")
-        fwrite.write(str(int(input())) + "\n")
-        fwrite.write(str(int(input())) + "\n")
+        fwrite.write(input("Enter start: ") + "\n")
+        fwrite.write(input("Enter end: ") + "\n")
+        fwrite.write(str(int(input("Enter depth: "))) + "\n")
+        fwrite.write(str(1) + "\n")
 
 
 if __name__ == "__main__":
