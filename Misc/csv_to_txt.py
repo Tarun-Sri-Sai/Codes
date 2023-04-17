7dde38a176e0a0c75e6b5d8e6a078a2ad4337e20
Tarun <tarunsrisaiv03@gmail.com>
2023-04-17 22:09:23 +0530

    Use isascii for removing UTF-8 characters

--- Misc/csv_to_txt.py
+++ Misc/csv_to_txt.py
@@ -1,14 +1,23 @@
 import os
-import codecs
+from typing import List
+
+
+def read_input_path(input_csv_path):
+    result: List[str] = []
+    with open(input_csv_path, "r", encoding="utf-8") as csv_in:
+        for line in csv_in.readlines():
+            line = line.strip()
+            result.append("".join(ch for ch in line if str.isascii(ch)))
+
+    return result
 
 
 def main():
     print(f"Cwd: {os.getcwd()}")
-    input_csv_path = input("Input .txt file path: ")
-    output_txt_path = input("Output .csv file path: ")
+    input_csv_path: str = input("Input .csv file path: ")
+    output_txt_path: str = input("Output .txt file path: ")
     with open(output_txt_path, "w") as txt_out:
-        csv_in = codecs.open(input_csv_path, "r", encoding="ascii", errors="ignore")
-        for line in csv_in:
+        for line in read_input_path(input_csv_path):
             txt_out.write(line + "\n")
 
 
