fab7535ce31cb209e466b963f31a7805bd70b8d9
Tarun <tarunsrisaiv03@gmail.com>
2023-04-18 05:35:37 +0530

    Wrap csv_to_txt

--- Misc/bpe/src/csv_to_txt.py
+++ Misc/bpe/src/csv_to_txt.py
@@ -9,7 +9,8 @@ def main() -> None:
         with open(input_csv_path, "r", encoding="utf-8") as csv_in:
             for line in csv_in.readlines():
                 line = line.strip().replace(",", " ").replace("  ", ", ")
-                txt_out.write("".join(ch for ch in line if str.isascii(ch)) + "\n")
+                txt_out.write(
+                    "".join(ch for ch in line if str.isascii(ch)) + "\n")
 
 
 if __name__ == "__main__":
