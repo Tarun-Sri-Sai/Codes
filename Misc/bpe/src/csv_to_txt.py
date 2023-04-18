652130ebca5938cf20a7cdbf53e36b8b1d50a533
Tarun <tarunsrisaiv03@gmail.com>
2023-04-18 05:34:45 +0530

    Edit csv_to_txt

--- Misc/bpe/src/csv_to_txt.py
+++ Misc/bpe/src/csv_to_txt.py
@@ -1,24 +1,15 @@
 import os
-from typing import List
 
 
-def read_input_path(input_csv_path):
-    result: List[str] = []
-    with open(input_csv_path, "r", encoding="utf-8") as csv_in:
-        for line in csv_in.readlines():
-            line = line.strip()
-            result.append("".join(ch for ch in line if str.isascii(ch)))
-
-    return result
-
-
-def main():
-    print(f"Cwd: {os.getcwd()}")
-    input_csv_path: str = input("Input .csv file path: ")
-    output_txt_path: str = input("Output .txt file path: ")
+def main() -> None:
+    print(f"CWD: {os.getcwd()}")
+    input_csv_path: str = input("Input .csv: ")
+    output_txt_path: str = input("Output .txt: ")
     with open(output_txt_path, "w") as txt_out:
-        for line in read_input_path(input_csv_path):
-            txt_out.write(line + "\n")
+        with open(input_csv_path, "r", encoding="utf-8") as csv_in:
+            for line in csv_in.readlines():
+                line = line.strip().replace(",", " ").replace("  ", ", ")
+                txt_out.write("".join(ch for ch in line if str.isascii(ch)) + "\n")
 
 
 if __name__ == "__main__":
