2ecc19f946ecf3b5bc5b1827a144bbc2ed91c1a2
Tarun <tarunsrisaiv03@gmail.com>
2023-04-18 20:16:54 +0530

    Remove quotation marks

--- Misc/bpe/src/csv_to_txt.py
+++ Misc/bpe/src/csv_to_txt.py
@@ -8,7 +8,7 @@ def main() -> None:
     with open(output_txt_path, "w") as txt_out:
         with open(input_csv_path, "r", encoding="utf-8") as csv_in:
             for line in csv_in.readlines():
-                line = line.strip().replace(",", " ").replace("  ", ", ")
+                line = line.strip().replace("\"", "").replace(",", " ").replace("  ", ", ")
                 txt_out.write(
                     "".join(ch for ch in line if str.isascii(ch)) + "\n")
 
