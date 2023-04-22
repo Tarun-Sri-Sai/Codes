ea32fb7305e231e0f3d69ca41c8ab07d1148de8a
Tarun <tarunsrisaiv03@gmail.com>
2023-04-22 23:33:33 +0530

    Change file path

--- Misc/markov/remove_non_ascii.py
+++ Misc/markov/remove_non_ascii.py
@@ -2,8 +2,8 @@ from string import printable
 
 
 def main():
-    with open("txt/train_ascii.txt", "w") as f_write:
-        with open("txt/train.txt", "r", encoding="utf-8") as f_read:
+    with open("txt/ice_and_fire_ascii.txt", "w") as f_write:
+        with open("txt/ice_and_fire.txt", "r", encoding="utf-8") as f_read:
             f_write.write(
                 "".join(filter(lambda ch: ch in printable, f_read.read())))
 
