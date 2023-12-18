b4ed706a30c7132bad0f86490fcad8b04cef23c1
Tarun <tarunsrisaiv03@gmail.com>
2023-12-18 18:00:41 +0530

    Add lines_of_code.py

---
+++ Misc/lines_of_code.py
@@ -0,0 +1,26 @@
+from os import path, listdir
+
+
+def recur_count(folder):
+    lines_of_code = 0
+    for item in listdir(folder):
+        item_path = path.join(folder, item)
+        if path.isdir(item_path):
+            lines_of_code += recur_count(item_path)
+        else:
+            file_loc = len(open(item_path, 'r', encoding='utf-8').readlines())
+            print(f'{item_path} lines of code: {file_loc}')
+            lines_of_code += file_loc
+    return lines_of_code
+
+
+def main():
+    lines_of_code = 0
+    while len(folder := input()) > 0:
+        lines_of_code += recur_count(folder)
+        print()
+    print(f'Total lines of code: {lines_of_code}')
+
+
+if __name__ == '__main__':
+    main()
