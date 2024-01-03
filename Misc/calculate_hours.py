e0f6455765fbd738bb3f4cf454088acef0507454
Tarun <tarunsrisaiv03@gmail.com>
2024-01-03 08:13:09 +0530

    Add print_html

--- Misc/calculate_hours.py
+++ Misc/calculate_hours.py
@@ -2,14 +2,15 @@ from argparse import ArgumentParser
 from markdown import markdown
 from re import findall
 from sys import exit
+from typing import Any
 
 
 class Parser:
     def __init__(self):
         self._parser = ArgumentParser(
             description='Find watch hours in markdown watchlists')
-        
-    def get_arguments(self) -> tuple[str, str | None, str | None]:
+
+    def get_arguments(self) -> dict[str, Any]:
         self._parser.add_argument(
             '-f',
             type=str,
@@ -18,26 +19,38 @@ class Parser:
         self._parser.add_argument(
             '-t',
             type=str,
-            dest='html_pattern',
-            help='Specify the HTML pattern containing the content info')
+            dest='text_pattern',
+            help='Specify the text pattern containing the content info')
         self._parser.add_argument(
             '-s',
             type=str,
             dest='split_pattern',
-            help='Pattern to split content types(Episode e, Movie m, Special s)')
-        
+            help='Specify the pattern to split content type and value')
+        self._parser.add_argument(
+            '-p',
+            action='store_true',
+            dest='print_html',
+            help='Print as HTML content')
+
         args = self._parser.parse_args()
 
         if args.md_file is None:
             self.exit_on_error('Bad md_file')
 
-        return args.md_file, args.html_pattern, args.split_pattern
-    
+        return {
+            'md_file': args.md_file,
+            'text_pattern': args.text_pattern,
+            'split_pattern': args.split_pattern,
+            'print_html': args.print_html
+        }
+
     def exit_on_error(self, error_string):
-        print(error_string, end='\n\n')
+        print(f'error: {error_string}', end='\n\n')
         self._parser.print_help()
         exit(1)
 
+    def exit_on_success(self):
+        exit(0)
 
 
 parser = Parser()
@@ -47,29 +60,20 @@ def to_html(md_text: str) -> str:
     return markdown(md_text)
 
 
-def find_content_info(html_content: str, html_pattern: str) -> list[str]:
+def find_content_info(html_content: str, text_pattern: str) -> list[str]:
     try:
-        return findall(html_pattern, html_content)
+        return findall(text_pattern, html_content)
     except Exception:
-        parser.exit_on_error('Bad html_pattern')
+        parser.exit_on_error('Bad text_pattern')
 
 
-def find_content_types(content_info: list[str],
-                       split_pattern: str) -> list[str]:
-
-    content_types = []
-    for content in content_info:
-        content_types += [s.strip() for s in content.split(split_pattern)]
-    return content_types
-
-
-def calculate_hours(content_types: list[str]) -> float:
+def calculate_hours(content_types: list[str], split_pattern: str) -> float:
     total_hours = 0
 
     try:
         multipliers = {'Episode': 1 / 3, 'Movie': 3 / 2, 'Special': 1}
         for item in content_types:
-            split_words = item.split(' ')
+            split_words = item.split(split_pattern)
             total_hours += int(split_words[1]) * multipliers[split_words[0]]
     except Exception:
         parser.exit_on_error('Bad split_pattern argument')
@@ -87,29 +91,32 @@ def format_hours(hours: float) -> str:
 
 
 def get_watch_time(md_text: str,
-                   html_pattern=None,
+                   text_pattern=None,
                    split_pattern=None) -> str:
 
-    html_pattern = html_pattern or r'<em>\[(.*?)\]</em>'
-    split_pattern = split_pattern or ', '
+    text_pattern = text_pattern or r'Episode \d+|Movie \d+|Special \d+'
+    split_pattern = split_pattern or ' '
 
     html_content = to_html(md_text)
-    content_info = find_content_info(html_content, html_pattern)
-    content_types = find_content_types(content_info, split_pattern)
-    hours = calculate_hours(content_types)
+    content_info = find_content_info(html_content, text_pattern)
+    hours = calculate_hours(content_info, split_pattern)
 
     return format_hours(hours)
 
 
 def main():
-    md_file, html_pattern, split_pattern = parser.get_arguments()
+    arguments = parser.get_arguments()
 
-    with open(md_file, 'r') as f:
+    with open(arguments['md_file'], 'r') as f:
         md_text = f.read()
 
+    if arguments['print_html']:
+        print(to_html(md_text))
+        parser.exit_on_success()
+
     print(get_watch_time(md_text,
-                         html_pattern=html_pattern,
-                         split_pattern=split_pattern))
+                         text_pattern=arguments['text_pattern'],
+                         split_pattern=arguments['split_pattern']))
 
 
 if __name__ == '__main__':
