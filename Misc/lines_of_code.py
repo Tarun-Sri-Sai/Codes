1919e77afc675c98619f8676b0dd50bdffb7e3fc
Tarun <tarunsrisaiv03@gmail.com>
2024-01-07 19:10:37 +0530

    lines_of_code: Fix match truthyness

--- Misc/lines_of_code.py
+++ Misc/lines_of_code.py
@@ -41,26 +41,26 @@ class LinesOfCode:
         }
         
     def _is_match(self, argument_key, string):
-        return bool(match(self._arguments[argument_key], string))
+        pattern = self._arguments[argument_key]
+        return pattern and match(pattern, string)
 
     def _get_loc(self, folder: str) -> int:
         lines_of_code = 0
         for item in listdir(folder):
             item_path = join(folder, item)
+
             if isdir(item_path):
                 if self._is_match('exclude_dirs', item_path):
                     continue
                 lines_of_code += self._get_loc(item_path)
-            else:
-                if self._is_match('exclude_exts', 
-                                  splitext(item)[1]):
-                    continue
-                if self._is_match('exclude_files', item_path):
-                    continue
-                lines_of_code += len(
-                    open(item_path, 
-                         encoding='utf-8',
-                         errors='ignore').readlines())          
+                continue
+
+            if self._is_match('exclude_exts', splitext(item)[1]):
+                continue
+            if self._is_match('exclude_files', item_path):
+                continue
+            lines_of_code += len(
+                open(item_path, encoding='utf-8', errors='ignore').readlines())        
 
         return lines_of_code
 
